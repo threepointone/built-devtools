edab36f51bd151778d59599010df33ac27a86d3c
Sunil Pai <spai@cloudflare.com>
2022-01-05 20:20:53 +0000

    Revert "make it actually work"
    This reverts commit eb1108718bd1781cd1f92950851eedf45a90c6a8.

--- front_end/wrangler-refresh.js
+++ front_end/wrangler-refresh.js
@@ -5,19 +5,16 @@ function sleep(period) {
 async function loop() {
   let value = -1;
   while (true) {
-    const ctr = await (await fetch(`http://localhost:3142`)).json();
+    const ctr = (await fetch(`http://localhost:3142`)).json().value;
     if (value !== -1) {
-      if (ctr.value !== value) {
+      if (ctr !== value) {
         location.reload();
-        break;
       } else {
         await sleep(1000);
       }
     } else {
-      value = ctr.value;
+      value = ctr;
       await sleep(1000);
     }
   }
 }
-
-loop();
